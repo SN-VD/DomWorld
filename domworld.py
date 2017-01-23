c14d2e9b33b665d35bfbfe4a53f07f4c659be6ea
Stephan <s.n.van.dijk.1@student.rug.nl>
2017-01-23 11:27:42 +0000

    Updated code

--- domworld.py
+++ domworld.py
@@ -2,4 +2,10 @@
 
 import os
 
+def domworld (*configs)
+	#G1 = raw_input("Do you want a graph of average dominance? y/n?")
+	#if G1 == 'y':
+		#MAverageDom = []
+	for ini in configs:
+		os.popen('wine cDomWorld.exe '+ ini)
 
