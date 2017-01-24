2311eca164610cf8b63e481bfa77b69f014e09b6
Stephan <s.n.van.dijk.1@student.rug.nl>
2017-01-24 09:33:39 +0000

    Updated code

--- domworld.py
+++ domworld.py
@@ -5,13 +5,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-def domworld (*configs)
+def domworld (*configs):
 	#G1 = raw_input("Do you want a graph of average dominance? y/n?")   #Ask for which graphs you want
 	allnames = []                                                       #Variables for creating graphs outside of the loop.
+	print ("Outside loop ok)
 	#if G1 == 'y':
 		#MAverageDom = []
 	for ini in configs:
 		numruns = os.popen("grep 'NumRuns' "+ ini +" | cut -d ' ' -f 3").read
+		print('inside loop ok')
 		maxperiod = os.popen("grep 'Periods' "+ ini +" | cut -d ' ' -f 3").read
 		startperiod = os.popen("grep 'firstDataPeriod' "+ ini +" | cut -d ' ' -f 3").read
 		periodduration = os.popen("grep 'PeriodDurationFactor' "+ ini +" | cut -d ' ' -f 3").read
@@ -21,4 +23,4 @@ def domworld (*configs)
 		totmales = os.popen("grep 'NumMales' "+ ini +" | cut -d ' ' -f 3").read
 		totchimps = totfemales + totmales
 		#os.popen('wine cDomWorld.exe '+ ini)                      #Run domworld, using ini as input
-		os.popen('cat '+ name +'*.csv > name.csv')
+		os.popen('cat '+ name +'*.csv > ' +name+'.csv')
